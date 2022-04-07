54de8b7618b002ae46644f595b35b91864650973
Brant McCarthy <brant.mccarthy@me.com>
2022-04-07 15:59:59 -0500

    react calendar demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Calendar from 'react-calendar'
+import React, { useState } from 'react';
+import Typography from '@mui/material/Typography'
 
 function App() {
+
+  const [calendarValue, setCalendarValue] = useState(new Date());
+
+  console.log('value:', calendarValue);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h2">Hello World.</Typography>
+        <br />
+        <Calendar onChange={setCalendarValue} value={calendarValue} />
+        <br />
+        <Typography variant="h5">{calendarValue.toString()}</Typography>
       </header>
     </div>
   );
